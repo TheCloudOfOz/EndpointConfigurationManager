4366579aa222e2974c0cba871de5451cdd58754e
Simon Oz <138634174+TheCloudOfOz@users.noreply.github.com>
2023-08-15 03:27:32 +1000

    Update Install.ps1

--- Microsoft/System Center/Endpoint Configuration Manager/Install.ps1
+++ Microsoft/System Center/Endpoint Configuration Manager/Install.ps1
@@ -1,1 +1,19 @@
+using namespace System.Management.Automation
+using namespace System.Net
 
+[ServicePointManager]::SecurityProtocol = [SecurityProtocolType]::Tls12
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned 
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+$Parameters = @{
+    Confirm      = $false 
+    Force        = $true
+    Scope        = "CurrentUser"
+    AllowClobber = $true
+    Repository   = "PSGallery"
+}
+# Microsoft
+Install-Module @Parameters -Name 'Microsoft.PowerShell.SecretManagement'
+Install-Module @Parameters -Name 'Microsoft.PowerShell.SecretStore'
+Install-Module @Parameters -Name 'Microsoft.Graph'
+Install-Module @Parameters -Name 'POSH-SSH'
