2f1dcfb084b48b7aa3a064e6a6ccfb74de63bcf9
Simon Oz <138634174+TheCloudOfOz@users.noreply.github.com>
2024-01-01 22:57:53 +1100

    Update Discovery.ps1

--- Microsoft/System Center/Endpoint Configuration Manager/Hierarchy Configuration/Discovery.ps1
+++ Microsoft/System Center/Endpoint Configuration Manager/Hierarchy Configuration/Discovery.ps1
@@ -1,1 +1,9 @@
-
+Get-CMDiscoveryMethod
+Get-CMDiscoveryMethod | Where-Object { $_.ComponentName -like "SMS_AD*" }
+Get-CMDiscoveryMethod | Where-Object { $_.ComponentName -like "SMS_AD*" } | Select-Object -Property ComponentName, Flag
+Get-CMDiscoveryMethod | Where-Object { $_.ComponentName -like "SMS_AD*" } | Select-Object ComponentName, @{l = "Status"; e = { switch ($_.Flag) {
+            2 { Write-Output "Disabled" }
+            6 { Write-Output "Enabled" }
+        }
+    }
+}
